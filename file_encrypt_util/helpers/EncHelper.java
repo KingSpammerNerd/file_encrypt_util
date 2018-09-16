c76bed6215ba1b622c7340df30d8dbb8e34f82af
KingSpammerNerd <kingspammernerd@gmail.com>
2018-09-16 22:11:17 +0530

    Change file storage method
    Compiles successfully, not yet tested.

--- file_encrypt_util/helpers/EncHelper.java
+++ file_encrypt_util/helpers/EncHelper.java
@@ -1,4 +1,4 @@
-package file_encrypt_helper.helpers;
+package file_encrypt_util.helpers;
 
 //Helper class to implement file encryption.
 import java.io.*;
@@ -8,107 +8,136 @@ import javax.crypto.*;
 import javax.crypto.spec.*;
 import java.util.*;
 
-//Requires: An input stream, output stream, Cipher, MessageDigest, Base64 encoder, File (for directory name)
+//Requires: An input stream, output stream, Cipher, MessageDigest, Base64 encoder.
+//Methods: constructor, readfile, hashfile, encryptfile, writefile.
 
 //Encryption helper class:
 class EncHelper {
 	//File input stream:
 	BufferedReader file_in=null;
 	//Encrypted file output streams:
-	BufferedWriter hash_out=null;
-	BufferedWriter enc_file_out=null;
-	//Cipher for encryption:
-	Cipher encryptor=null;
+	BufferedWriter file_out=null;
+	//Base64 encoded contents of encrypted file:
+	String enc64=null;
 	//MessageDigest for hashing:
 	MessageDigest hasher256=null;
+	//Base64 encoded SHA-256 hash of original file:
+	String hashed64=null;
 	//Base64 encoder:
 	Base64.Encoder b64_enc=null;
+	//File name:
+	String orig_file_name=null;
+	//Initialization Vector:
+	String IV64=null;
+	
+	//Supplied encryption key:
+	String passwd=null;
 	
 	//Original file contents:
 	String orig_file_contents=null;
 	
-	//Constructor, takes a file name and a password for encryption:
-	public EncHelper(String filename, String pass) throws IOException {
-		//Open file input stream:
-		this.file_in=new BufferedReader(new FileReader(new File(filename)));
-		//Create destination files:
-		this.createFiles();
+	//Target file contents:
+	String target_file_contents=null;
+	
+	//Constructor, takes a file name and an encryption key:
+	public EncHelper(String orig_file_name, String passkey) throws IOException {
+		//Save filename:
+		this.orig_file_name=orig_file_name;
+		//Open input stream:
+		this.file_in=new BufferedReader(new FileReader(orig_file_name));
+		//Save password:
+		this.passwd=passkey;
 		//Create Base64 encoder:
 		this.b64_enc=Base64.getEncoder();
 	}
 	
-	//Created target files:
-	private void createFiles() throws IOException, RuntimeException {
-		//Open encrypted file output and base64 hash file output:
-		StringTokenizer efo=new StringTokenizer(filename, ".");
-		String efo_filename=efo.nextToken();
-		
-		//Create target directory, throw RuntimeException if it already exists:
-		if(new File(efo_filename).exists()) {
-			Scanner s=new Scanner(System.in);
-			System.out.print("Error: directory " + '"' + efo_filename + '"' + " already exists! Overwrite? [y/n]: ");
-			char ch=s.nextLine().charAt(0);
-			s.close();
-			if(!(ch=='Y' || ch=='y')) throw new RuntimeException("ERROR_TARGET_FOLDER_EXISTS");
+	//Function to read and store input file's contents:
+	public void readInput() throws IOException {
+		//Iteratively read through file, line by line:
+		StringBuffer contents=new StringBuffer();
+		String temp=null;
+		while((temp=file_in.readLine())!=null) {
+			contents.append(temp);
+			contents.append('\n');
 		}
-		
-		//Create target directory:
-		File target_dir=new File(efo_filename).createDirectory();
-		//Create files:
-		File hash_file=new File(efo_filename + "/" + enc_filename + ".sha256");
-		File enc_file=new File(efo_filename + "/" + efo_filename + ".enc");
-		
-		//Open file output streams:
-		this.hash_out=new BufferedWriter(new FileWriter(hash_file));
-		this.enc_file_out=new BufferedWriter(new FileWriter(enc_file));
-	}
-	
-	//Reads contents of file to memory:
-	private void readFile() throws IOException {
-		String temp=null; StringBuffer con=new StringBuffer();
-		
-		//Read contents, with newlines:
-		while(temp=this.file_in.readLine()) {
-			con.append(temp);
-			con.append('\n');
-		}
-		
 		//Remove trailing newline:
-		con.deleteCharAt(con.length()-1);
-		
-		//Store contents:
-		this.orig_file_contents=con.toString();
+		contents.deleteCharAt(contents.length()-1);
+		//Save contents:
+		this.orig_file_contents=contents.toString();
 	}
 	
-	//Hashes file:
-	public void doHash() throws IOException {
-		//Create MessageDigest:
+	//Function to hash the file:
+	public void hashInput() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		//Initialize MessageDigest:
 		this.hasher256=MessageDigest.getInstance("SHA-256");
-		
-		//Hash text:
-		byte[] hash=this.hasher256.digest(orig_file.getBytes("UTF-8"));
-		
-		//Encode hash to Base64:
-		byte[] hash64=b64_enc.encode(hash);
-		String hashed64=new String(hash64, "UTF-8");
-		
-		//Write hash to file:
-		hash_out.write(hashed64);
+		//Hash and store:
+		byte[] hash64=this.hasher256.digest(this.orig_file_contents.getBytes("UTF-8"));
+		this.hashed64=this.b64_enc.encodeToString(hash64);
 	}
 	
-	//Encrypts file:
-	public void doEncrypt() {
-		//Create Cipher:
-		this.
+	//Function to encrypt the file:
+	public void encryptInput() throws NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		//Create random number generator:
+		SecureRandom sr=new SecureRandom();
+		byte[] iv=new byte[16];
+		//Create Cipher for encryption:
+		Cipher encrypter=Cipher.getInstance("AES/CBC/PKCS5Padding");
+		//Create SecretKeySpec:
+		SecretKeySpec key=new SecretKeySpec(this.passwd.getBytes("UTF-8"), "AES");
+		//Initialize Cipher:
+		sr.nextBytes(iv);
+		encrypter.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+		//Store Initialization Vector:
+		this.IV64=b64_enc.encodeToString(iv);
+		//Encrypt contents:
+		byte[] enc=encrypter.doFinal(this.orig_file_contents.getBytes("UTF-8"));
+		this.enc64=new String(enc, "UTF-8");
 	}
 	
-	//Close all file streams (call from main!):
+	//Function to write and verify the output file (returns true if file is "good", else returns false. Should NEVER return false during normal usage):
+	public boolean writeOutput() throws IOException {
+		//Open output file:
+		File out=new File(this.orig_file_name + ".fenc");
+		//Throw exception if file exists. The program should prompt the user at this point:
+		if(out.exists()) throw new IOException("OUT_FILE_EXISTS");
+		//Open the file:
+		this.file_out=new BufferedWriter(new FileWriter(out));
+		//Concatenate the data. First the Initialization vector (this.IV64), then the encrypted contents (this.enc64), finally the SHA-256 hash of the encrypted file (this.hashed64):
+		StringBuffer out_builder=new StringBuffer();
+		out_builder.append(this.IV64);
+		out_builder.append('\n');
+		out_builder.append(this.enc64);
+		out_builder.append('\n');
+		out_builder.append(this.hashed64);
+		this.target_file_contents=out_builder.toString();
+		//Hash the concatenated contents:
+		byte[] prehash=this.hasher256.digest(this.target_file_contents.getBytes("UTF-8"));
+		String prehash64=this.b64_enc.encodeToString(prehash);
+		//Write data to the file:
+		this.file_out.write(target_file_contents);
+		this.file_out.flush();
+		//Re-read data from file:
+		BufferedReader check_reader=new BufferedReader(new FileReader(out));
+		String temp=null; StringBuilder checc=new StringBuilder();
+		while((temp=check_reader.readLine())!=null) {
+			checc.append(temp);
+			checc.append('\n');
+		}
+		checc.deleteCharAt(checc.length()-1);
+		String check=checc.toString();
+		//Hash re-read data:
+		byte[] posthash=this.hasher256.digest(check.getBytes("UTF-8"));
+		String posthash64=this.b64_enc.encodeToString(posthash);
+		//Check if hashes match:
+		if(prehash64.equals(posthash64))
+			return true;
+		else
+			return false; //Once again, this should NEVER HAPPEN!
+	}
+	
+	//Function to close all streams:
 	public void finish() throws IOException {
-		//Close input stream:
-		file_in.close();
-		//Close hash output stream:
-		hash_out.close();
-		//Close enc output stream:
-		enc_file_out.close();
+		this.file_in.close();
+		this.file_out.close();
 	}
 }
